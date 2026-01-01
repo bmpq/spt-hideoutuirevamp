f65a51fa26a3568d344b81abf8dd0c98106e3111
bmpq <73074311+bmpq@users.noreply.github.com>
2026-01-01 22:49:51 +0300

    init

---
+++ Patch_AreasPanel_Awake.cs
@@ -0,0 +1,56 @@
+﻿using EFT.Hideout;
+using HarmonyLib;
+using SPT.Reflection.Patching;
+using System;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace tarkin.huir
+{
+    internal class Patch_AreasPanel_Awake : ModulePatch
+    {
+        private static readonly BepInEx.Logging.ManualLogSource Logger = BepInEx.Logging.Logger.CreateLogSource(nameof(Patch_AreasPanel_Awake));
+
+        protected override MethodBase GetTargetMethod()
+        {
+            return AccessTools.Method(typeof(AreasPanel), nameof(AreasPanel.Awake));
+        }
+
+        [PatchPostfix]
+        private static void PatchPostfix(AreasPanel __instance,
+            Button ____leftButton, 
+            Button ____rightButton,
+            AreasScrollRect ____scrollView)
+        {
+            try
+            {
+                ____leftButton.gameObject.SetActive(false);
+                ____rightButton.gameObject.SetActive(false);
+
+                RectTransform rect = __instance.transform as RectTransform;
+                rect.pivot = new Vector2(0, 0);
+                rect.anchorMin = new Vector2(0, 0);
+                rect.anchorMax = new Vector2(0, 1);
+                rect.anchoredPosition = Vector2.zero;
+                rect.offsetMin = new Vector2(0, 30f);
+                rect.offsetMax = new Vector2(400, 0);
+
+                ____scrollView.vertical = true;
+                ____scrollView.horizontal = false;
+                ____scrollView.movementType = ScrollRect.MovementType.Clamped;
+                ____scrollView.scrollSensitivity = 50;
+
+                ____scrollView.viewport.anchorMin = new Vector2(0, 0);
+                ____scrollView.viewport.anchorMax = new Vector2(1, 1);
+                ____scrollView.viewport.offsetMin = new Vector2(0, 0);
+                ____scrollView.viewport.offsetMax = new Vector2(0, 0);
+
+                Component.DestroyImmediate(____scrollView.content.GetComponent<HorizontalLayoutGroup>());
+                ____scrollView.content.gameObject.AddComponent<VerticalLayoutGroup>();
+                ____scrollView.content.pivot = new Vector2(0, 1); 
+            }
+            catch (Exception e) { Logger.LogError(e); }
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,19 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using System.IO;
+using UnityEngine;
+
+namespace tarkin.huir
+{
+    [BepInPlugin("com.tarkin.huir", MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+    internal class Plugin : BaseUnityPlugin
+    {
+        internal static ManualLogSource Log = new ManualLogSource(MyPluginInfo.PLUGIN_NAME);
+
+        private void Start()
+        {
+            new Patch_AreasPanel_Awake().Enable();
+        }
+    }
+}
