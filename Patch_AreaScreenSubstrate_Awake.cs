3dff7d90b11b849bb7c93795451b9187dd966b13
bmpq <73074311+bmpq@users.noreply.github.com>
2026-01-02 06:17:19 +0300

    thinner header caption

--- Patch_AreaScreenSubstrate_Awake.cs
+++ Patch_AreaScreenSubstrate_Awake.cs
@@ -18,12 +18,24 @@ namespace tarkin.huir
         }
 
         [PatchPostfix]
-        private static void PatchPostfix(AreaScreenSubstrate __instance, LayoutElement ____contentLayout, ref float ____maxHeight)
+        private static void PatchPostfix(AreaScreenSubstrate __instance, 
+            ref float ____maxHeight,
+            AreaPanel ____areaPanel)
         {
             try
             {
                 __instance.RectTransform.anchoredPosition = new Vector2(__instance.RectTransform.anchoredPosition.x, 60f);
                 ____maxHeight = 850f;
+
+                ____areaPanel.RectTransform.anchorMin = new Vector2(0, 0.5f);
+                ____areaPanel.RectTransform.anchorMax = new Vector2(0, 0.5f);
+                ____areaPanel.RectTransform.anchoredPosition = new Vector2(15f, 0);
+                ____areaPanel.transform.parent.GetComponent<LayoutElement>().preferredHeight = 37f;
+
+                ____areaPanel.AreaName.fontSize = 18f;
+                ____areaPanel.AreaName.fontStyle = TMPro.FontStyles.Bold;
+
+                __instance.RectTransform.Find("Border").SetSiblingIndex(1);
             }
             catch (Exception e) { Logger.LogError(e); }
         }
