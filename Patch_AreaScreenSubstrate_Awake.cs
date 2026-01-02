75e32d87af92e849915e22792ee9139c0bf65ac7
bmpq <73074311+bmpq@users.noreply.github.com>
2026-01-02 06:47:42 +0300

    header icon hexagon opaque background
    looks nicer

--- Patch_AreaScreenSubstrate_Awake.cs
+++ Patch_AreaScreenSubstrate_Awake.cs
@@ -36,8 +36,38 @@ namespace tarkin.huir
                 ____areaPanel.AreaName.fontStyle = TMPro.FontStyles.Bold;
 
                 __instance.RectTransform.Find("Border").SetSiblingIndex(1);
+
+                CreateSimpleFilledHexagon(____areaPanel.Container, 37f, Color.black);
             }
             catch (Exception e) { Logger.LogError(e); }
+        }
+
+        private static void CreateSimpleFilledHexagon(RectTransform parent, float sideSize, Color color)
+        {
+            void CreateRect(Vector2 size, float rotationZ)
+            {
+                GameObject go = new GameObject("hexagon part");
+                go.transform.SetParent(parent, false);
+
+                Image img = go.AddComponent<Image>();
+                img.color = color;
+
+                RectTransform rt = go.GetComponent<RectTransform>();
+                rt.sizeDelta = size;
+                rt.localEulerAngles = new Vector3(0, 0, rotationZ);
+
+                rt.anchorMin = new Vector2(0.5f, 0.5f);
+                rt.anchorMax = new Vector2(0.5f, 0.5f);
+                rt.pivot = new Vector2(0.5f, 0.5f);
+                rt.anchoredPosition = Vector2.zero;
+            }
+
+            float shortDim = sideSize;
+            float longDim = sideSize * Mathf.Sqrt(3f);
+
+            CreateRect(new Vector2(shortDim, longDim), -30f);
+            CreateRect(new Vector2(shortDim, longDim), 30f);
+            CreateRect(new Vector2(longDim, shortDim), 0f);
         }
     }
 }
